a7d4dc4d61ae1c371fd8fefd35a59c2879abf07a
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-10-26 21:18:36 -0300

    Add files via upload

---
+++ Lista3_EX3_PRCA2/Lista3_EX3_PRCA2/Quadrado.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_EX3_PRCA2
+{
+    class Quadrado
+    {
+        private int d;
+        private int area;
+
+        public Quadrado()
+        {
+            d = 0;
+            area = 0;
+        }
+
+        public Quadrado(int d, int area)
+        {
+            this.d = d;
+            this.area = area;
+        }
+        public void setDiagonal(int d)
+        {
+            this.d = d;
+        }
+        public int getDiagonal()
+        {
+            return this.d;
+        }
+        public int getArea()
+        {
+            return this.area;
+        }
+
+        public void resultado()
+        {
+            this.area = (int)((Math.Pow(this.d, 2)) / 2);
+        }
+    }
+}
