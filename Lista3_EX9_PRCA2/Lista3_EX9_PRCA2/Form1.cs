c9bb6ca2fd33a9dd528686fa7d96755fdf62a06b
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-10-26 23:32:09 -0300

    Add files via upload

---
+++ Lista3_EX9_PRCA2/Lista3_EX9_PRCA2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista3_EX9_PRCA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Numeros numero;
+            numero = new Numeros();
+            numero.setN1(int.Parse(textBox1.Text));
+            numero.setN2(int.Parse(textBox2.Text));
+            if (numero.getN1() > numero.getN2())
+            {
+                MessageBox.Show("O maior número é " + numero.getN1());
+            }
+            if (numero.getN1() < numero.getN2())
+            {
+                MessageBox.Show("O maior número é " + numero.getN2());
+            }
+           
+            if (numero.getN1() == numero.getN2())
+            {
+                MessageBox.Show("Os números são iguais");
+            }
+        }
+
+    }
+}
---
+++ Lista3_EX9_PRCA2/Lista3_EX9_PRCA2/Numeros.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_EX9_PRCA2
+{
+    class Numeros
+    {
+        private int n1;
+        private int n2;
+
+        public Numeros()
+        {
+            n1 = 0;
+            n2 = 0;
+        }
+        public Numeros(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+        public void setN1(int n1)
+        {
+            this.n1 = n1;
+        }
+        public int getN1()
+        {
+            return this.n1;
+        }
+        public void setN2(int n2)
+        {
+            this.n2 = n2;
+        }
+        public int getN2()
+        {
+            return this.n2;
+        }
+    }
+}
