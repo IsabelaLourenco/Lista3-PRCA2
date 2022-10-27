9df9d71eb236778e0e93d7657195ceab480cbbc5
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-10-26 23:44:52 -0300

    Add files via upload

---
+++ Lista3_EX10_PRCA2/Lista3_EX10_PRCA2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista3_EX10_PRCA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Terreno ter;
+            ter = new Terreno();
+            ter.setB(int.Parse(textBox1.Text));
+            ter.setH(int.Parse(textBox2.Text));
+            ter.area();
+            if (ter.getA() > 100)
+            {
+                MessageBox.Show("Terreno grande com área de  "
+                    + ter.getA());
+            }
+            if (ter.getA() < 100)
+            {
+                MessageBox.Show("Terreno pequeno com área de  "
+                    + ter.getA());
+            }
+        }
+    }
+}
---
+++ Lista3_EX10_PRCA2/Lista3_EX10_PRCA2/Terreno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_EX10_PRCA2
+{
+    class Terreno
+    {
+        private int b;
+        private int h;
+        private int a;
+
+        public Terreno()
+        {
+            b = 0;
+            h = 0;
+            a = 0;
+        }
+
+        public Terreno(int b, int h, int a)
+        {
+            this.b = b;
+            this.h = h;
+            this.a = a;
+        }
+
+        public void setB(int b)
+        {
+            this.b = b;
+        }
+        public void setH(int h)
+        {
+            this.h = h;
+        }
+
+        public int getA()
+        {
+            return this.a;
+        }
+        public void area()
+        {
+            this.a = this.b * this.h;
+        }
+    }
+}
