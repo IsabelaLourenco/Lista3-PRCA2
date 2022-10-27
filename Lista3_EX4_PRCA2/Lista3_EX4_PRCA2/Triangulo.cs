4d218d45c15fc6505dbdecf142cc4412ee6b400a
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-10-26 21:37:51 -0300

    Add files via upload

---
+++ Lista3_EX4_PRCA2/Lista3_EX4_PRCA2/Triangulo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_EX4_PRCA2
+{
+    class Triangulo
+    {
+       
+            private int b;
+            private int h;
+            private int a;
+
+            public Triangulo()
+            {
+                b = 0;
+                h = 0;
+                a = 0;
+            }
+
+            public Triangulo(int a, int h, int b)
+            {
+                this.b = b;
+                this.h = h;
+                this.a = a;
+            }
+            public void setB(int b)
+            {
+                this.b = b;
+            }
+            public void setH(int h)
+            {
+                this.h = h;
+            }
+            public int getB()
+            {
+                return this.b;
+            }
+            public int getH()
+            {
+                return this.h;
+            }
+            public int getA()
+            {
+                return this.a;
+            }
+            public void area()
+            {
+                this.a = (this.b * this.h) / 2;
+            }
+
+        }
+    }
+
