8c72f845b92949358faddcad78a331267ff5ad9c
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-10-26 23:58:24 -0300

    Add files via upload

---
+++ Lista_EX11_PRCA2/Lista_EX11_PRCA2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_EX11_PRCA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Peso peso;
+            peso = new Peso();
+            peso.setPeso(Double.Parse(textBox1.Text));
+            peso.setAltura(Double.Parse(textBox2.Text));
+            peso.indice();
+            if(peso.getIMC() < 20)
+            {
+                MessageBox.Show("Você está abaixo do peso");
+            }
+            if (20 <= peso.getIMC() && peso.getIMC() < 25)
+            {
+                MessageBox.Show("Você está com o peso ideal");
+            }
+            if (peso.getIMC() > 25)
+            {
+                MessageBox.Show("Você está acima do peso");
+            }
+        }
+    }
+}
---
+++ Lista_EX11_PRCA2/Lista_EX11_PRCA2/Peso.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_EX11_PRCA2
+{
+    class Peso
+    {
+        private double altura;
+        private double peso;
+        private double imc;
+
+        public Peso()
+        {
+            altura = 0;
+            peso = 0;
+            imc = 0;
+        }
+        public Peso(double altura, double peso, double imc)
+        {
+            this.altura = altura;
+            this.peso = peso;
+            this.imc = imc;
+        }
+        public void setAltura(double altura)
+        {
+            this.altura = altura;
+        }
+        public double getAltura()
+        {
+            return this.altura;
+        }
+        public void setPeso(double peso)
+        {
+            this.peso = peso;
+        }
+        public double getPeso()
+        {
+            return this.peso;
+        }
+        public double getIMC()
+        {
+            return this.imc;
+        }
+        public void indice()
+        {
+            this.imc = this.peso / (this.altura * this.altura);
+        }
+    }
+}
