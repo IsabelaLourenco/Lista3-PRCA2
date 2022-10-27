e4efc5b4d231436a6c5131e27b64d41b6ce66aed
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-10-27 00:13:54 -0300

    Add files via upload

---
+++ Lista3_EX12_PRCA2/Lista3_EX12_PRCA2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista3_EX12_PRCA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Triangulo tri;
+            tri = new Triangulo();
+            tri.setA(int.Parse(textBox1.Text));
+            tri.setB(int.Parse(textBox2.Text));
+            tri.setC(int.Parse(textBox3.Text));
+            MessageBox.Show(tri.ehTriangulo() ?
+                "é um triângulo" :
+                "não é um triângulo") ;
+            MessageBox.Show(tri.ehEquilatero() ?
+               "é um triangulo equilatero":
+               "") ;
+            MessageBox.Show(tri.ehIsoceles() ?
+                "é um triangulo isoceles" :
+                "");
+
+            MessageBox.Show(tri.ehEscaleno() ?
+                "é um triângulo escaleno" :
+                "");
+        }
+    }
+}
---
+++ Lista3_EX12_PRCA2/Lista3_EX12_PRCA2/Triangulo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_EX12_PRCA2
+{
+    class Triangulo
+    {
+        private int a;
+        private int b;
+        private int c;
+
+        public Triangulo()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+
+        public Triangulo(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+        public void setB(int b)
+        {
+            this.b = b;
+        }
+        public void setC(int c)
+        {
+            this.c = c;
+        }
+        public int getA()
+        {
+            return this.a;
+        }
+        public int getB()
+        {
+            return this.b;
+        }
+        public int getC()
+        {
+            return this.c;
+        }
+
+        public bool ehTriangulo()
+        {
+            return (
+                this.a + this.b > this.c ||
+                this.a + this.c > this.b ||
+                this.b + this.c > this.a
+
+                );
+        }
+        public bool ehEscaleno()
+        {
+            return (
+                  this.a != this.b &&
+                  this.b != this.c &&
+                  this.c != this.a
+                   );
+        }
+
+        public bool ehEquilatero()
+        {
+            return (
+                    this.a == this.b &&
+                    this.c == this.b &&
+                    this.a == this.c
+                    );
+        }
+        public bool ehIsoceles()
+        {
+            return (
+             this.a == this.b &&
+             this.a != this.c ||
+             this.b != this.c &&
+             this.b == this.c &&
+             this.b != this.a ||
+             this.c != this.a &&
+             this.c == this.a &&
+             this.c != this.b ||
+             this.a != this.b
+              );
+        }
+    }
+}
