f4baefc94c9a6303b19d758c4331034135c4b62b
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-10-26 23:13:21 -0300

    Add files via upload

---
+++ Lista3_EX8_PRCA2/Lista3_EX8_PRCA2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista3_EX8_PRCA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Numeros numero;
+            numero = new Numeros();
+            numero.setN1(int.Parse(textBox1.Text));
+            numero.setN2(int.Parse(textBox2.Text));
+            if (numero.getN1() > numero.getN2())
+            {
+                MessageBox.Show("O maior valor é  " + numero.getN1());
+            }
+            else
+            {
+                MessageBox.Show("O maior valor é  " + numero.getN2());
+
+            }
+        }
+    }
+}
