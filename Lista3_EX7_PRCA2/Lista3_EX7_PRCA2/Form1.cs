d9af804bb34a1c68af87cc19183a22df7453f997
IsabelaLourenco <113075880+IsabelaLourenco@users.noreply.github.com>
2022-10-26 23:02:39 -0300

    Add files via upload

---
+++ Lista3_EX7_PRCA2/Lista3_EX7_PRCA2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista3_EX7_PRCA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Moeda moeda;
+            moeda = new Moeda();
+            moeda.setCotacao(Double.Parse(textBox1.Text));
+            moeda.setDolar(Double.Parse(textBox2.Text));
+            moeda.resultado();
+            MessageBox.Show( moeda.getDolar()+ " dolares equivalem a " 
+               + moeda.getReal() + " reais");
+        }
+    }
+}
---
+++ Lista3_EX7_PRCA2/Lista3_EX7_PRCA2/Moeda.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_EX7_PRCA2
+{
+    class Moeda
+    {
+        private double dolar;
+        private double cotacao;
+        private double real;
+
+        public Moeda()
+        {
+            dolar = 0;
+            cotacao = 0;
+            real = 0;
+        }
+
+        public Moeda(double dolar, double cotacao, double real)
+        {
+            this.dolar = dolar;
+            this.cotacao = cotacao;
+            this.real = real;
+        }
+
+        public void setDolar(double dolar)
+        {
+            this.dolar = dolar;
+        }
+        public double getDolar()
+        {
+            return this.dolar;
+        }
+        public void setCotacao(double cotacao)
+        {
+            this.cotacao = cotacao;
+        }
+        public double getCotacaor()
+        {
+            return this.cotacao;
+        }
+        public double getReal()
+        {
+            return this.real;
+        }
+
+        public void resultado()
+        {
+            this.real = this.cotacao * this.dolar;
+        }
+    }
+}
